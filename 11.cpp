bae2f3f1b97d7b6b8ead4678954a2aa3db85fe3e
HAHA <854034863@qq.com>
2018-01-26 19:14:17 +0800

    I won't see you tonight so I can keep from going insance
    翻译：今晚我不会见你 这样我才能保持安静
    2018-1-26 19：14 修改顺序

--- 11.cpp
+++ 11.cpp
@@ -1,25 +1,30 @@
 /*
-给定一个double类型的浮点数base和int类型的整数exponent。
-求base的exponent次方.
+输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示
 */
 #include<iostream>
+#include<vector>
 using namespace std;
-int Power(double base,int exponent)
+//位运算，将1每次左移，和数字进行&运算，如果成功，则返回1
+int NumberOf1(int  n)
 {
-	if(exponent==0)
-		return 1;
-	else if(exponent<0)
-		return 1/base*(Power(base,exponent+1));
-	else
-		return base*(Power(base,exponent-1));
+	int result=0;
+	int index=1;
+	while(index!=0)
+	{
+        if((n & index)!=0)
+            result++;
+        index = index << 1;
+    }
+	return result;
 }
+//将整数通过方法转换为二进制数，然后统计其中1的数量
+
 int main()
 {
-	double base;
-	int exponent;
-	cin >> base >> exponent ;
-	int result=Power(base,exponent);
+	int n;
+	cin >> n;
+	int result;
+	result = NumberOf1(n);
 	cout << result << endl;
-
 	return 0;
 }
